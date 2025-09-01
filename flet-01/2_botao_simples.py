ea932baa68496cf49c2dfb7209aba4d9e0bc302d
julia-daniluski <julia.o.daniluski@gmail.com>
2025-09-01 15:15:07 -0300

    ok

--- flet-01/2_botao_simples.py
+++ flet-01/2_botao_simples.py
@@ -1,10 +1,9 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page. title = "Meu Primeiro Botão"
+    page.title = "Meu primeiro Botão"
     page.padding = 20
 
-    # Criando um texto que será modificado pelo botão
     mensagem = ft.Text(
         value="Clique no botão abaixo!",
         size=20,
@@ -12,25 +11,25 @@ def main(page: ft.Page):
     )
 
     def botao_clicado(evento):
-        mensagem. value =" Parabens! Voce clicou no botão!"
-        mensagem. color = ft.Colors.GREEN
-
-# IMPORTANTE: Sempre que modificamos elementos da interface,
-# precisamos chamar page.update() para que as mudanças apareçam na tela
-    page. update()
+        # Mudando o texto da mensagem
+        mensagem.value = "Parabéns! Você clicou no botão!"
+        mensagem.color = ft.Colors.GREEN
+        # IMPORTANTE: Sempre que modificamos elementos da interface,
+        # precisamos chamar page.update() para que as mudanças apareçam na tela
+        page.update()
 
     # Criando nosso botão
-    meu_botao = ft. ElevatedButton(
-        text="Clique em mim!",
-        on_click=botao_clicado, # Função que será executada no clique
-        width=200, # Largura do botão
-        height=50, # Altura do botão
-        bgcolor=ft.Colors.BLUE, # Cor de fundo
-        color=ft.Colors.WHITE # Cor do texto
+    meu_botao = ft.ElevatedButton(
+        text="Clique em mim!",          # Texto que aparece no botão
+        on_click=botao_clicado,         # Função que será executada no clique
+        width=200,                      # Largura do botão
+        height=50,                      # Altura do botão
+        bgcolor=ft.Colors.BLUE,         # Cor de fundo
+        color=ft.Colors.WHITE           # Cor do texto
     )
 
-
-    page. add (mensagem)
-    page. add (meu_botao)
+    # Adicionando os elementos à página
+    page.add(mensagem)
+    page.add(meu_botao)
 
 ft.app(target=main)
